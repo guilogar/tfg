ec7a9aaf1f22f952de7ec695894135ac7ec83d8b
Guillermo López García <lopezgarciaguillermo@live.com>
2021-03-07 02:13:15 +0100

    emulator add to azure

---
+++ emulator/read-messages.js
@@ -0,0 +1,93 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT Licence.
+
+/*
+  This sample demonstrates how to use the Microsoft Azure Event Hubs Client for JavaScript to 
+  read messages sent from a device. Please see the documentation for @azure/event-hubs package
+  for more details at https://www.npmjs.com/package/@azure/event-hubs
+
+  For an example that uses checkpointing, follow up this sample with the sample in the 
+  eventhubs-checkpointstore-blob package on GitHub at the following link:
+
+  https://github.com/Azure/azure-sdk-for-js/blob/master/sdk/eventhub/eventhubs-checkpointstore-blob/samples/javascript/receiveEventsUsingCheckpointStore.js
+*/
+
+const { EventHubConsumerClient } = require("@azure/event-hubs");
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+// If using websockets, uncomment the following require statement
+// const WebSocket = require("ws");
+
+// If you need proxy support, uncomment the below code to create proxy agent
+// const HttpsProxyAgent = require("https-proxy-agent");
+// const proxyAgent = new HttpsProxyAgent(proxyInfo);
+
+// Event Hub-compatible endpoint
+// az iot hub show --query properties.eventHubEndpoints.events.endpoint --name {your IoT Hub name}
+const eventHubsCompatibleEndpoint = process.env.EVENTHUBS_COMPATIBLE_ENDPOINT;
+
+// Event Hub-compatible name
+// az iot hub show --query properties.eventHubEndpoints.events.path --name {your IoT Hub name}
+const eventHubsCompatiblePath = process.env.EVENTHUBS_COMPATIBLE_PATH;
+
+// Primary key for the "service" policy to read messages
+// az iot hub policy show --name service --query primaryKey --hub-name {your IoT Hub name}
+const iotHubSasKey = process.env.IOTHUB_SAS_KEY;
+
+// If you have access to the Event Hub-compatible connection string from the Azure portal, then
+// you can skip the Azure CLI commands above, and assign the connection string directly here.
+const connectionString = 
+    `Endpoint=${eventHubsCompatibleEndpoint};` +
+    `EntityPath=${eventHubsCompatiblePath};` +
+    `SharedAccessKeyName=service;` + 
+    `SharedAccessKey=${iotHubSasKey}`;
+
+var printError = function (err) {
+  console.log(err.message);
+};
+
+// Display the message content - telemetry and properties.
+// - Telemetry is sent in the message body
+// - The device can add arbitrary properties to the message
+// - IoT Hub adds system properties, such as Device Id, to the message.
+var printMessages = function (messages) {
+  for (const message of messages) {
+    console.log("Telemetry received: ");
+    console.log(JSON.stringify(message.body));
+    console.log("Properties (set by device): ");
+    console.log(JSON.stringify(message.properties));
+    console.log("System properties (set by IoT Hub): ");
+    console.log(JSON.stringify(message.systemProperties));
+    console.log("");
+  }
+};
+
+async function main() {
+  console.log("IoT Hub Quickstarts - Read device to cloud messages.");
+
+  // If using websockets, uncomment the webSocketOptions below
+  // If using proxy, then set `webSocketConstructorOptions` to { agent: proxyAgent }
+  // You can also use the `retryOptions` in the client options to configure the retry policy
+  const clientOptions = {
+    // webSocketOptions: {
+    //   webSocket: WebSocket,
+    //   webSocketConstructorOptions: {}
+    // }
+  };
+
+  // Create the client to connect to the default consumer group of the Event Hub
+  const consumerClient = new EventHubConsumerClient("$Default", connectionString, clientOptions);
+
+  // Subscribe to messages from all partitions as below
+  // To subscribe to messages from a single partition, use the overload of the same method.
+  consumerClient.subscribe({
+    processEvents: printMessages,
+    processError: printError,
+  });
+}
+
+main().catch((error) => {
+  console.error("Error running sample:", error);
+});
---
+++ emulator/simulator.js
@@ -0,0 +1,54 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+'use strict';
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+// The device connection string to authenticate the device with your IoT hub.
+//
+// NOTE:
+// For simplicity, this sample sets the connection string in code.
+// In a production environment, the recommended approach is to use
+// an environment variable to make it available to your application
+// or use an HSM or an x509 certificate.
+// https://docs.microsoft.com/azure/iot-hub/iot-hub-devguide-security
+//
+// Using the Azure CLI:
+// az iot hub device-identity show-connection-string --hub-name {YourIoTHubName} --device-id MyNodeDevice --output table
+var connectionString = process.env.CONNECTION_STRING;
+
+// Using the Node.js Device SDK for IoT Hub:
+//   https://github.com/Azure/azure-iot-sdk-node
+// The sample connects to a device-specific MQTT endpoint on your IoT Hub.
+var Mqtt = require('azure-iot-device-mqtt').Mqtt;
+var DeviceClient = require('azure-iot-device').Client
+var Message = require('azure-iot-device').Message;
+
+var client = DeviceClient.fromConnectionString(connectionString, Mqtt);
+
+// Create a message and send it to the IoT hub every second
+setInterval(function(){
+  // Simulate telemetry.
+  var temperature = 20 + (Math.random() * 15);
+  var message = new Message(JSON.stringify({
+    temperature: temperature,
+    humidity: 60 + (Math.random() * 20)
+  }));
+
+  // Add a custom application property to the message.
+  // An IoT hub can filter on these properties without access to the message body.
+  message.properties.add('temperatureAlert', (temperature > 30) ? 'true' : 'false');
+
+  console.log('Sending message: ' + message.getData());
+
+  // Send the message.
+  client.sendEvent(message, function (err) {
+    if (err) {
+      console.error('send error: ' + err.toString());
+    } else {
+      console.log('message sent');
+    }
+  });
+}, 1000);
