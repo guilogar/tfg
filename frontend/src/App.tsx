5bf7e5fcc9307e5420833cfd80f85d4b9eaf5f01
Guillermo López García <lopezgarciaguillermo@live.com>
2021-04-25 20:54:15 +0200

    check session time env add

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -49,7 +49,7 @@ const App: React.FC = () => {
   };
 
   useEffect(() => {
-    setInterval(checkSession, 10000);
+    setInterval(checkSession, Number(process.env.REACT_APP_CHECK_SESSION_TIME));
   });
 
   const AlertSession = withRouter(({ history }) => (
