4dca0cc3f5ccc916fb2e03cfec750e26b4ef73af
Guillermo López García <lopezgarciaguillermo@live.com>
2021-05-14 23:48:04 +0200

    update cron tab and service worker in frontend

---
+++ backend/cron-tab.js
@@ -0,0 +1,98 @@
+// # ┌────────────── second (optional)
+// # │ ┌──────────── minute
+// # │ │ ┌────────── hour
+// # │ │ │ ┌──────── day of month
+// # │ │ │ │ ┌────── month
+// # │ │ │ │ │ ┌──── day of week
+// # │ │ │ │ │ │
+// # │ │ │ │ │ │
+// # * * * * * *
+const cron = require('node-cron');
+const {
+  sendNotificationToUser
+} = require('./routes/services/send-notification-to-user');
+const {
+  getAllEventFromCosmos, removeEventsFromCosmos
+} = require('./routes/services/get-event-from-cosmos');
+const {
+  checkEvent
+} = require('./routes/services/check-event');
+
+const UserSensor = require('./database/models/UserSensor');
+const Event = require('./database/models/Event');
+const Notification = require('./database/models/Notification');
+
+const ACTIONS = require('./routes/constans/event-actions');
+
+const initCronTab = () => {
+  cron.schedule('* * * * *', async () => {
+    try
+    {
+      const items = await getAllEventFromCosmos();
+      const eventsFired = await checkEvent(items);
+      for (const eventFired of eventsFired)
+      {
+        const {
+          userId, sensorId, eventId,
+          userEventId, value, action
+        } = eventFired;
+
+        const userSensor = await UserSensor.findOne({
+          where: {
+            id: sensorId
+          }
+        });
+
+        const event = await Event.findOne({
+          where: {
+            id: eventId
+          }
+        });
+
+        if(action === 'AUTOMATIC') {
+          const actionFunction = ACTIONS[event.name];
+          await actionFunction();
+        }
+
+        const body = (action === 'AUTOMATIC' ?
+          `El Evento ${event.name} ha sido disparado con el ` +
+          `sensor "${userSensor.name}" y el valor ${value}. ` +
+          `Se ha realizado la accion automatizada.`
+          :
+          `El Evento ${event.name} ha sido disparado con el ` +
+          `sensor "${userSensor.name}" y el valor ${value}. ` +
+          `Clicke aquí para realizar la acción asociada al evento.`);
+
+        const notification = {
+          title: `Evento ${event.name}`,
+          body: body
+        };
+
+        await Notification.create({
+          title: notification.title,
+          body: notification.body,
+          UserId: userId
+        });
+
+        try {
+          const response = await sendNotificationToUser(userId, notification);
+          console.log(response);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+
+      try {
+        await removeEventsFromCosmos(items);
+      } catch (error) {
+        console.log(error);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  });
+};
+
+module.exports = {
+  initCronTab
+};
--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -2,8 +2,12 @@ importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
 
 (async () => {
-  const response = await fetch('firebase-credentials.json');
-  firebaseCredentials = await response.json();
-  firebase.initializeApp(firebaseCredentials);
-  firebase.messaging();
+  try {
+    const response = await fetch('firebase-credentials.json');
+    firebaseCredentials = await response.json();
+    firebase.initializeApp(firebaseCredentials);
+    firebase.messaging();
+  } catch (error) {
+    console.log(error);
+  }
 })();
